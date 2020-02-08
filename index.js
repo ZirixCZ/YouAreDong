896f7389fda79c2e84b0b53868f5c25f234ec481
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2020-02-08 21:41:32 +0100

    New commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function kamaratdownload() {
+    window.open("https://mega.nz/#!j0UmyS7Q!Oa3eBF3kr5gBIw7bIxdbQIFgw8Hc886LT4ydJyor4v4")
+}
+
+function sampdownload() {
+    window.open("https://mega.nz/#!ysswwAqY!Hi8gfKic41uKAs2d0QEaU4ttW2HksXGq8Gfxfw-Vrkg")
+}
+function gtasadownlod() {
+    window.open("https://mega.nz/#!6gsR2QyS!QCGsXbMgqqbqaRWLf-axm-QQu23dLquA-nHeu-f2LC0")
+}
